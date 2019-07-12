e207c730719a6d120d9cdb9115a48e82804e0ca5
alce65 <alce65@hotmail.es>
2019-07-12 10:38:27 +0200

    Enrutamiento inicial y uso de bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home', 
+    loadChildren: () => import('./home/home.module')
+      .then(m => m.HomeModule)
+  },
+  { path: 'tareas', loadChildren: './tareas/tareas.module#TareasModule' },
+  { path: 'usuarios', loadChildren: './usuarios/usuarios.module#UsuariosModule' },
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: '**', redirectTo: 'home'  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
